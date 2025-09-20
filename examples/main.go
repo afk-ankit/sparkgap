a7faace0d936d211adc9b7e4acaf253797e300dc
Ankit Sharma <ankitsharmagh093@gmail.com>
2025-09-20 17:24:56 +0530

    Add: example to test half-state

--- examples/main.go
+++ examples/main.go
@@ -20,8 +20,11 @@ func main() {
 	go func() {
 		time.Sleep(time.Second * 5)
 		broke = true
+		time.Sleep(time.Second * 30)
+		broke = false
 	}()
 	for range 1000 {
+		time.Sleep(time.Second * 1)
 		val, err := br.Execute(func() (string, error) { return accounts("ankit", broke) })
 		if err != nil {
 			fmt.Print(err.Error())
