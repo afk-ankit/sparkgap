63fab7e44f19c2924e72a60bd3d72881c6d24b9a
Ankit Sharma <ankitsharmagh093@gmail.com>
2025-09-20 17:24:33 +0530

    Add: halfState core logic

--- breaker/breaker.go
+++ breaker/breaker.go
@@ -17,11 +17,9 @@ const (
 )
 
 type Counter struct {
-	Success          uint32
-	Failure          uint32
-	FailureThrehold  uint32
-	TimeoutThreshold time.Time
-	RetryThreshold   time.Time
+	Failure         uint32
+	FailureThrehold uint32
+	RetryDuration   time.Duration
 }
 
 type Breaker[T any] struct {
@@ -30,8 +28,38 @@ type Breaker[T any] struct {
 	State   int
 }
 
+func (br *Breaker[T]) startRetry() {
+	go func() {
+		time.Sleep(br.Counter.RetryDuration)
+		br.State = HalfOpen
+	}()
+}
+
 func (br *Breaker[T]) Execute(fn func() (T, error)) (T, error) {
 	var zero T
+	switch br.State {
+	case Open:
+		return zero, fmt.Errorf("circuit breaker is open")
+	case HalfOpen:
+		res, err := fn()
+		if err != nil {
+			fmt.Println("Rety Unsuccessfull")
+			br.failure()
+			return res, err
+		}
+		atomic.StoreUint32(&br.Counter.Failure, 0)
+		fmt.Println("Retry Successfull")
+		br.State = Closed
+		return res, nil
+	case Closed:
+		res, err := fn()
+		if err != nil {
+			br.failure()
+			return res, err
+		}
+		return res, nil
+	}
+
 	if br.State == Open {
 		return zero, fmt.Errorf("circuit breaker is open")
 	}
@@ -40,18 +68,17 @@ func (br *Breaker[T]) Execute(fn func() (T, error)) (T, error) {
 		br.failure()
 		return res, err
 	}
-	br.success()
 	return res, nil
 }
 
-func (br *Breaker[T]) success() {
-	atomic.AddUint32(&br.Counter.Success, 1)
-}
-
 func (br *Breaker[T]) failure() {
+	if br.State == HalfOpen {
+		br.State = Open
+	}
 	atomic.AddUint32(&br.Counter.Failure, 1)
 	if br.Counter.Failure >= br.Counter.FailureThrehold {
 		br.State = Open
+		br.startRetry()
 	}
 }
 
@@ -60,6 +87,7 @@ func InitBreaker[T any](name string) *Breaker[T] {
 		Name: name,
 		Counter: Counter{
 			FailureThrehold: 10,
+			RetryDuration:   time.Second * 5,
 		},
 		State: Closed,
 	}
